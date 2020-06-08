15cd9640db2da5c6a2a895869f72715c120a8da1
satoyuki <yuki01377@gmail.com>
2020-06-08 14:47:19 +0900

    auth0 create

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
 
 const app = express();
 
@@ -41,8 +43,21 @@ app.get('/:id', (req, res) => {
   res.send(question[0]);
 });
 
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://dev-hp-9boc4.auth0.com/.well-known/jwks.json`
+  }),
+  // Validate the audience and the issuer.
+  audience: '2wSDgOkWjT4YbKqLyTtz4IyVPbypTG5P',
+  issuer: 'https://dev-hp-9boc4.auth0.com/',
+  algorithms: ['RS256']
+})
+
 // insert a new question
-app.post('/', (req, res) => {
+app.post('/', checkJwt, (req, res) => {
   console.log(req.body);
   const { title, description } = req.body;
   const newQuestion = {
@@ -50,19 +65,21 @@ app.post('/', (req, res) => {
     title,
     description,
     answers: [],
+    author: req.user.name,
   };
   questions.push(newQuestion);
   res.status(200).send();
 });
 
 // insert a new answer to a question
-app.post('/answer/:id', (req, res) => {
+app.post('/answer/:id', checkJwt, (req, res) => {
   const { answer } = req.body;
   const question = questions.filter(q => (q.id === parseInt(req.params.id)));
   if (question.length > 1) return res.status(500).send();
   if (question.length === 0) return res.status(404).send();
   question[0].answers.push({
     answer,
+    author: req.user.name,
   });
   res.send(question[0]);
 });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { Route } from 'react-router-dom';
 import NavBar from './NavBar/NavBar';
 import Questions from './Questions/Questions';
 import Question from './Question/Question';
+import Callback from './Callback';
+
 // functionの場合render()はいらない
 class App extends Component {
   render() {
@@ -11,7 +13,8 @@ class App extends Component {
         <NavBar />
         {/* <Questions/> */}
         <Route exact path="/" component={Questions} />
-        <Route exact path="/question/:questionId" component={Question}/>
+        <Route exact path="/question/:questionId" component={Question} />
+        <Route exact path="/callback" component={Callback} />
       </div>
     )
   }
